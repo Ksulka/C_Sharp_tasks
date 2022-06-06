6eb22bd07b3bcdd90d1fdc47b3b16f9e8b29119b
xoxana <1008484@mail.ru>
2022-06-06 22:06:52 +0300

    Задача на проверку, является ли первое число квадратом второго

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine("Да, первое число является квадратом второго");
+}
+else 
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
