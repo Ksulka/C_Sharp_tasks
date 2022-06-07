df52778c3428af947f3ca0d315f2a8cf53ce3e38
xoxana <1008484@mail.ru>
2022-06-07 23:10:28 +0300

    Программа, которая на выходе показывает все четные числа до задуманного числа

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 8: Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+// 5 -> 2, 4
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int start = 2;
+if (N < 2)
+{
+    Console.WriteLine(" до этого числа нет четных чисел");
+}
+if (N % 2 == 0)
+{
+    Console.Write(N + " -->   ");
+    while (start <= N)
+    {
+        Console.Write(start);
+        Console.Write("  ");
+    start = start + 2;
+    }
+}
+else
+{   
+    Console.Write(N + " -->   ");
+    while (start <= N-1)
+    {
+        Console.Write(start);
+        Console.Write("  ");
+    start = start + 2;
+    }
+}
