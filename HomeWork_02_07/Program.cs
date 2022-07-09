3ba5f2dac8f52162fda54749365134c411d1b5c9
xoxana <1008484@mail.ru>
2022-07-09 14:27:50 +0300

    Домашнее задание к 8 семинару и задания с 10 семинара

---
+++ HomeWork_02_07/Program.cs
@@ -0,0 +1,215 @@
+﻿
+Console.WriteLine("Домашнее задание к семинару №8");
+
+// Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i,j] / 10 <= 0)
+      Console.Write($" {arr[i,j]} ");
+
+      else Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] CreateArray(int n, int m)
+{
+    int[,] array = new int[n, m];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+
+    }
+    return array;
+}
+
+
+int[,] massiv = (CreateArray(5, 6));
+
+PrintArray(massiv);
+Console.WriteLine("");
+
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    int sorted = 0;
+    int help = 0;
+    while (sorted < massiv.GetLength(1))
+    {
+        int j = 0;
+        while (j < massiv.GetLength(1) - 1 - sorted)
+        {
+            if (massiv[i, j] < massiv[i, j + 1])
+            {
+                help = massiv[i, j + 1];
+                massiv[i, j + 1] = massiv[i, j];
+                massiv[i, j] = help;
+            }
+            j++;
+        }
+        sorted++;
+    }
+
+}
+PrintArray(massiv);
+Console.WriteLine("");
+
+// Задайте прямоугольный двумерный массив. Напишите
+// программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] array = (CreateArray(5, 6));
+
+PrintArray(array);
+Console.WriteLine("");
+int[] sum = new int[array.GetLength(0)];
+for (int i = 0; i < sum.Length; i++)
+{
+    for (int k = 0; k < array.GetLength(1); k++)
+    {
+        sum[i] = sum[i] + array[i, k];
+    }
+}
+int min = sum[0];
+int indexmin = 0;
+for (int i = 0; i < sum.Length; i++)
+{
+    if (sum[i] < min)
+    {
+        min = sum[i];
+        indexmin = i;
+    }
+}
+
+Console.WriteLine($"Минимальная сумма элементов у строки с индексом {indexmin}  и она равна  {min}");
+Console.WriteLine();
+// Задайте две матрицы. Напишите программу, которая будет
+// находить произведение двух матриц.
+
+int[,] matrix1 = (CreateArray(5, 6));
+PrintArray(matrix1);
+Console.WriteLine("");
+int[,] matrix2 = (CreateArray(6, 5));
+PrintArray(matrix2);
+Console.WriteLine("");
+
+int[,] matrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+for (int i = 0; i < matrix1.GetLength(0); i++)
+{
+
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        matrix[i, j] = matrix1[i, j] * matrix2[j, i];
+    }
+
+}
+PrintArray(matrix);
+Console.WriteLine("");
+
+
+// Сформируйте трёхмерный массив из неповторяющихся
+// двузначных чисел. Напишите программу, которая будет построчно выводить
+// массив, добавляя индексы каждого элемента.
+
+Console.WriteLine("Введите ширину");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите высоту");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите глубину");
+int z = int.Parse(Console.ReadLine());
+
+int[,,] cube = new int[x, y, z];
+
+int[] random = new int[cube.Length];
+int number;
+for (int i = 0; i < random.Length; i++)
+{
+    random[i] = new Random().Next(10, 100);
+    number = random[i];
+    if (i >= 1)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            while (random[i] == random[j])
+            {
+                random[i] = new Random().Next(10, 100);
+                j = 0;
+                number = random[i];
+            }
+            number = random[i];
+        }
+    }
+}
+
+for (int j = 0; j < random.Length; j++)
+{
+Console.Write(random[j] + " ");
+}
+Console.WriteLine();
+int count = 0;
+
+for (int i = 0; i < cube.GetLength(0); i++)
+{
+    for (int j = 0; j < cube.GetLength(1); j++)
+    {
+        for (int k = 0; k < cube.GetLength(2); k++)
+        {
+            cube[i, j, k] = random[count];
+            count++;
+        }
+    }
+}
+
+for (int i = 0; i < cube.GetLength(0); i++)
+{
+    for (int j = 0; j < cube.GetLength(1); j++)
+    {
+        
+        for (int k = 0; k < cube.GetLength(2); k++)
+        {
+            Console.WriteLine($"({i}, {j}, {k}) = {cube[i, j, k]}; ");
+        }
+        
+    }
+   
+}
+Console.WriteLine();
+// Заполните спирально массив 4 на 4.
+
+// 1  2  3  4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9  8  7
+
+int[,] spiral = new int[4,4];
+
+int temp = 1;
+int s = 0;
+int w = 0;
+
+while (temp <= spiral.GetLength(0) * spiral.GetLength(1))
+{
+  spiral[s, w] = temp;
+  temp++;
+  if (s <= w + 1 && s + w < spiral.GetLength(1) - 1)
+    w++;
+  else if (s < w && s + w >= spiral.GetLength(0) - 1)
+    s++;
+  else if (s >= w && s + w > spiral.GetLength(1) - 1)
+    w--;
+  else
+    s--;
+}
+
+PrintArray(spiral);
---
+++ task13/Program.cs
@@ -0,0 +1,101 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+
+// Напишите программу, которая на вход принимает два числа и выдаёт
+// первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// 3 и 4, N = 5 -> 3 4 7 11 18
+// 6 и 10, N = 4 -> 6 10 16 26
+
+int  RecFib(int first, int second, int num)
+{
+    if(num<=0) return 0;
+    if(num==1) return first;
+    if(num==2) return second;
+    return RecFib(first, second, num-1) + RecFib(first, second, num-2);
+}
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество");
+int N = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= N; i++)
+{
+    Console.Write($"{RecFib(a,b,i)}  ");
+}
+
+// В некотором машинном алфавите имеются четыре буквы «а», «и», «с»
+// и «в». Покажите все слова, состоящие из n букв, которые можно построить из
+// букв этого алфавита.
+// n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са,
+// ав, ва, ви, ив, св, вс
+
+
+string[] Words(string alpha, int length)
+{
+    string[] result;
+    if (length <= 0)
+    {
+        result = new string[1];
+        result[0] = String.Empty;
+        return result;
+    }
+
+    string[] resultOld = Words(alpha, length - 1);
+
+
+    result = new string[resultOld.Length * alpha.Length];
+    int count = 0;
+    for (int i = 0; i < resultOld.Length; i++)
+    {
+        for (int j = 0; j < alpha.Length; j++)
+        {
+            result[count] = resultOld[i] + alpha[j];
+            count++;
+        }
+    }
+
+    return result;
+}
+Console.WriteLine(" ");
+
+int n = 4;
+string alphabet = "пион";
+string[] word;
+word = Words(alphabet, n);
+
+for (int i = 0; i < word.Length; i++)
+{
+    Console.Write((i + 1) + " " + word[i] + " ");
+}
+
+Console.WriteLine(" ");
+// Заданы 2 массива: info и data. В массиве info хранятся двоичные
+// представления нескольких чисел (без разделителя). В массиве data хранится
+// информация о количестве бит, которые занимают числа из массива info.
+// Напишите программу, которая составит массив десятичных представлений чисел
+// массива data с учётом информации из массива info.
+// 25 мин
+// входные данные:
+// - data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+// - info = {2, 3, 3, 1 }
+// выходные данные:
+// - 1, 7, 0, 1
+int [] data = {0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int [] info = {2, 3, 3, 1 };
+int sum = 0;
+int mult;
+for (int i = 0; i < info.Length; i++)
+{
+    mult = 0;
+    for (int j = sum; j < sum + info[i]; j++)
+    {
+        mult += data[j] * (int)Math.Pow(2, info[i] - j + sum - 1);
+        
+    }
+    sum += info[i];
+    Console.WriteLine(mult);
+    Console.WriteLine(" ");
+}
